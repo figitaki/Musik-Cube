08235bfad90e842be997dfd551cdba8dab2f58ab
Carey Janecka <figitaki96@yahoo.com>
2015-12-06 20:00:06 -0600

    Initial Commit

---
+++ Musik Cube/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Musik Cube
+//
+//  Created by Carey Janecka on 12/6/15.
+//  Copyright © 2015 Carey Janecka. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
